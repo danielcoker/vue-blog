2a31fdea65b43856cdca4900d022aa47c98128f6
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-04-24 19:37:37 +0100

    Create NavBar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="success">
+      <b-container>
+        <b-navbar-brand href="#">Blog</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-collapse id="nav-collapse" is-nav>
+              <b-navbar-nav>
+                <b-nav-item :to="{ name: 'Home' }">Home</b-nav-item>
+                <b-nav-item :to="{ name: 'About' }">About</b-nav-item>
+                <b-nav-item href="#">Posts</b-nav-item>
+              </b-navbar-nav>
+            </b-collapse>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+};
+</script>
