959ba82f0b5cc2d5c01d3e1ac47fccbc30727f19
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-04-24 19:36:24 +0100

    Add Home and About views

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,119 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About My Blog</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis,
+      elit et mollis porta, orci sem congue risus, id pellentesque neque sem
+      vitae ante. Cras porta augue nec urna viverra, eleifend ornare nisi
+      mattis. Proin at augue consequat, tempus mi aliquam, luctus augue. Vivamus
+      luctus cursus porta. Maecenas rutrum accumsan justo, at volutpat neque.
+      Aenean efficitur dui sit amet arcu feugiat, nec posuere eros dapibus.
+      Curabitur ut leo nec dui feugiat commodo. Nullam sollicitudin blandit
+      augue, quis rutrum dolor hendrerit eu. Class aptent taciti sociosqu ad
+      litora torquent per conubia nostra, per inceptos himenaeos. Phasellus
+      bibendum eleifend urna, efficitur tempor nisi congue commodo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis,
+      elit et mollis porta, orci sem congue risus, id pellentesque neque sem
+      vitae ante. Cras porta augue nec urna viverra, eleifend ornare nisi
+      mattis. Proin at augue consequat, tempus mi aliquam, luctus augue. Vivamus
+      luctus cursus porta. Maecenas rutrum accumsan justo, at volutpat neque.
+      Aenean efficitur dui sit amet arcu feugiat, nec posuere eros dapibus.
+      Curabitur ut leo nec dui feugiat commodo. Nullam sollicitudin blandit
+      augue, quis rutrum dolor hendrerit eu. Class aptent taciti sociosqu ad
+      litora torquent per conubia nostra, per inceptos himenaeos. Phasellus
+      bibendum eleifend urna, efficitur tempor nisi congue commodo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis,
+      elit et mollis porta, orci sem congue risus, id pellentesque neque sem
+      vitae ante. Cras porta augue nec urna viverra, eleifend ornare nisi
+      mattis. Proin at augue consequat, tempus mi aliquam, luctus augue. Vivamus
+      luctus cursus porta. Maecenas rutrum accumsan justo, at volutpat neque.
+      Aenean efficitur dui sit amet arcu feugiat, nec posuere eros dapibus.
+      Curabitur ut leo nec dui feugiat commodo. Nullam sollicitudin blandit
+      augue, quis rutrum dolor hendrerit eu. Class aptent taciti sociosqu ad
+      litora torquent per conubia nostra, per inceptos himenaeos. Phasellus
+      bibendum eleifend urna, efficitur tempor nisi congue commodo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis,
+      elit et mollis porta, orci sem congue risus, id pellentesque neque sem
+      vitae ante. Cras porta augue nec urna viverra, eleifend ornare nisi
+      mattis. Proin at augue consequat, tempus mi aliquam, luctus augue. Vivamus
+      luctus cursus porta. Maecenas rutrum accumsan justo, at volutpat neque.
+      Aenean efficitur dui sit amet arcu feugiat, nec posuere eros dapibus.
+      Curabitur ut leo nec dui feugiat commodo. Nullam sollicitudin blandit
+      augue, quis rutrum dolor hendrerit eu. Class aptent taciti sociosqu ad
+      litora torquent per conubia nostra, per inceptos himenaeos. Phasellus
+      bibendum eleifend urna, efficitur tempor nisi congue commodo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis,
+      elit et mollis porta, orci sem congue risus, id pellentesque neque sem
+      vitae ante. Cras porta augue nec urna viverra, eleifend ornare nisi
+      mattis. Proin at augue consequat, tempus mi aliquam, luctus augue. Vivamus
+      luctus cursus porta. Maecenas rutrum accumsan justo, at volutpat neque.
+      Aenean efficitur dui sit amet arcu feugiat, nec posuere eros dapibus.
+      Curabitur ut leo nec dui feugiat commodo. Nullam sollicitudin blandit
+      augue, quis rutrum dolor hendrerit eu. Class aptent taciti sociosqu ad
+      litora torquent per conubia nostra, per inceptos himenaeos. Phasellus
+      bibendum eleifend urna, efficitur tempor nisi congue commodo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis,
+      elit et mollis porta, orci sem congue risus, id pellentesque neque sem
+      vitae ante. Cras porta augue nec urna viverra, eleifend ornare nisi
+      mattis. Proin at augue consequat, tempus mi aliquam, luctus augue. Vivamus
+      luctus cursus porta. Maecenas rutrum accumsan justo, at volutpat neque.
+      Aenean efficitur dui sit amet arcu feugiat, nec posuere eros dapibus.
+      Curabitur ut leo nec dui feugiat commodo. Nullam sollicitudin blandit
+      augue, quis rutrum dolor hendrerit eu. Class aptent taciti sociosqu ad
+      litora torquent per conubia nostra, per inceptos himenaeos. Phasellus
+      bibendum eleifend urna, efficitur tempor nisi congue commodo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis,
+      elit et mollis porta, orci sem congue risus, id pellentesque neque sem
+      vitae ante. Cras porta augue nec urna viverra, eleifend ornare nisi
+      mattis. Proin at augue consequat, tempus mi aliquam, luctus augue. Vivamus
+      luctus cursus porta. Maecenas rutrum accumsan justo, at volutpat neque.
+      Aenean efficitur dui sit amet arcu feugiat, nec posuere eros dapibus.
+      Curabitur ut leo nec dui feugiat commodo. Nullam sollicitudin blandit
+      augue, quis rutrum dolor hendrerit eu. Class aptent taciti sociosqu ad
+      litora torquent per conubia nostra, per inceptos himenaeos. Phasellus
+      bibendum eleifend urna, efficitur tempor nisi congue commodo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis,
+      elit et mollis porta, orci sem congue risus, id pellentesque neque sem
+      vitae ante. Cras porta augue nec urna viverra, eleifend ornare nisi
+      mattis. Proin at augue consequat, tempus mi aliquam, luctus augue. Vivamus
+      luctus cursus porta. Maecenas rutrum accumsan justo, at volutpat neque.
+      Aenean efficitur dui sit amet arcu feugiat, nec posuere eros dapibus.
+      Curabitur ut leo nec dui feugiat commodo. Nullam sollicitudin blandit
+      augue, quis rutrum dolor hendrerit eu. Class aptent taciti sociosqu ad
+      litora torquent per conubia nostra, per inceptos himenaeos. Phasellus
+      bibendum eleifend urna, efficitur tempor nisi congue commodo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis,
+      elit et mollis porta, orci sem congue risus, id pellentesque neque sem
+      vitae ante. Cras porta augue nec urna viverra, eleifend ornare nisi
+      mattis. Proin at augue consequat, tempus mi aliquam, luctus augue. Vivamus
+      luctus cursus porta. Maecenas rutrum accumsan justo, at volutpat neque.
+      Aenean efficitur dui sit amet arcu feugiat, nec posuere eros dapibus.
+      Curabitur ut leo nec dui feugiat commodo. Nullam sollicitudin blandit
+      augue, quis rutrum dolor hendrerit eu. Class aptent taciti sociosqu ad
+      litora torquent per conubia nostra, per inceptos himenaeos. Phasellus
+      bibendum eleifend urna, efficitur tempor nisi congue commodo.
+    </p>
   </div>
 </template>
+
+<script>
+export default {
+  title: 'About - My Blog',
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <PostList />
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PostList from '@/components/PostList';
 
 export default {
   name: 'Home',
+  title: 'Home - My Blog',
   components: {
-    HelloWorld
-  }
-}
+    PostList,
+  },
+};
 </script>
