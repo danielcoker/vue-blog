3c72c9676fd2d49703df83080d5b086da2c4047e
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-04-24 19:38:02 +0100

    Get and display posts from API

---
+++ src/components/PostList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <TheHero />
+
+    <b-container>
+      <div v-if="posts.length">
+        <b-row class="mt-5">
+          <b-col md="4" v-bind:key="post.id" v-for="post in posts">
+            <PostListItem v-bind:post="post" />
+          </b-col>
+        </b-row>
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import TheHero from '@/components/TheHero';
+import PostListItem from '@/components/PostListItem';
+import axios from 'axios';
+
+export default {
+  name: 'PostList',
+  components: {
+    TheHero,
+    PostListItem
+  },
+  data() {
+    return {
+      posts: []
+    };
+  },
+  created() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts?_limit=12')
+      .then(response => {
+        this.posts = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+};
+</script>
---
+++ src/components/PostListItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-card tag="article" style="max-width: 20rem;" class="mb-2">
+    <b-card-text>{{ post.body.slice(0, 100) }}</b-card-text>
+    <b-button href="#" variant="success">View Post</b-button>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'PostListItem',
+  props: ['post']
+};
+</script>
