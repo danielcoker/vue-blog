cda79e16ecab962aaed1a0204c8c049c05734ba5
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-04-24 19:37:27 +0100

    Create Hero component

---
+++ src/components/TheHero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <b-jumbotron
+      header="Welcome To My Blog"
+      lead="A test blog powered by Vuejs and JSONPlaceholder as API"
+    >
+      <b-button variant="success" href="#">All Posts</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHero',
+};
+</script>
