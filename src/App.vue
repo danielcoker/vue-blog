e9a72ba5b3b4cdd4d84fc71e55f6b51d2be0f495
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-04-24 19:35:28 +0100

    Replace router-view with NavBar component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <NavBar />
+    <router-view />
   </div>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar';
+
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
 
 <style>
 #app {
